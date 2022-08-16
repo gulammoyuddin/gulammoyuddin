0ac52b8c11f2c60266dc11f399d81e7861ac893f
Gulammoyuddin <gulammohyuddinmo.shafique@gmail.com>
2022-08-16 23:40:22 +0530

    Exercise_1.2

---
+++ part_1/course_info/src/App.js
@@ -0,0 +1,49 @@
+const Part = (props) =>{
+  return (
+    <div>
+      <p>{props.name} {props.number}</p>
+    </div>
+  )
+}
+const Header =(props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Content =(props) =>{
+  return (
+    <div>
+      <Part name={props.p1} number={props.e1}/>
+      <Part name={props.p2} number={props.e2}/>
+      <Part name={props.p3} number={props.e3}/>
+    </div>
+  )
+}
+const Total = (props) =>{
+  return (
+    <div>
+      <p> Number of exercises {props.e1+props.e2+props.e3}</p>
+    </div>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content p1={part1} p2={part2} p3={part3} e1={exercises1} e2={exercises2} e3={exercises3}/>
+      <Total e1={exercises1} e2={exercises2} e3={exercises3}/>
+    </div>
+  )
+}
+
+export default App
